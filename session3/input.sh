57d3e50b93a88f23fed4f0e7963837059f7ab7f3
Vedanshu Nishad <vedanshun05@gmail.com>
2026-08-18 16:58:18 +0530

    added input file

---
+++ session3/input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter your name: " name
+read -p "Enter your roll number: " roll_no
+read -p "Enter comment": comment
+
+echo "My name is $name"
+echo "My roll number is $roll_no"
+echo "Comment: $comment"
